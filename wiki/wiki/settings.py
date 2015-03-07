0453272e6f8b7e5e05369a42f20d4286e8721f74
kimiyoung <kimiyoung@yeah.net>
2015-03-07 11:38:48 +0800

    shrink range, max depth, bfs

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -13,28 +13,13 @@ BOT_NAME = 'wiki'
 SPIDER_MODULES = ['wiki.spiders']
 NEWSPIDER_MODULE = 'wiki.spiders'
 
-DOWNLOAD_DELAY = 0.2
+DOWNLOAD_DELAY = 0.1
+
+DEPTH_PRIORITY = 1
+SCHEDULER_DISK_QUEUE = 'scrapy.squeue.PickleFifoDiskQueue'
+SCHEDULER_MEMORY_QUEUE = 'scrapy.squeue.FifoMemoryQueue'
+
+DEPTH_LIMIT = 7
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'wiki (+http://www.yourdomain.com)'
-
-'''
-# Retry many times since proxies often fail
-RETRY_TIMES = 10
-# Retry on most error codes since proxies fail for different reasons
-RETRY_HTTP_CODES = [500, 503, 504, 400, 403, 404, 408]
-
-DOWNLOADER_MIDDLEWARES = {
-    'scrapy.contrib.downloadermiddleware.retry.RetryMiddleware': 90,
-    # Fix path to this module
-    'wiki.randomproxy.randomproxy.RandomProxy': 100,
-    'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110,
-}
-'''
-
-# Proxy list containing entries like
-# http://host1:port
-# http://username:password@host2:port
-# http://host3:port
-# ...
-PROXY_LIST = 'proxy_list.txt'
--- wiki/wiki/spiders/wiki_spider.py
+++ wiki/wiki/spiders/wiki_spider.py
@@ -6,7 +6,15 @@ from wiki.items import WikiItem
 class WikiSpider(scrapy.Spider):
     name = 'wiki'
     allowed_domains = ['wikipedia.org']
-    start_urls = ['http://en.wikipedia.org/wiki/Category:Subfields_by_academic_discipline']
+    start_urls = ['http://en.wikipedia.org/wiki/Category:Areas_of_computer_science',
+    'http://en.wikipedia.org/wiki/Category:Engineering_disciplines']
+
+    def check(self, s):
+        inputs = s.split()
+        if inputs[0].isupper(): return False
+        if any(inputs[i][0].isupper() for i in range(1, len(inputs))): return False
+        if any(char.isdigit() for char in s): return False
+        return True
 
     def parse(self, response):
         soup = BeautifulSoup(response.body)
@@ -15,16 +23,17 @@ class WikiSpider(scrapy.Spider):
         if soup.title.string.split(':')[0] != 'Category':
             yield Request(url = response.url, dont_filter = True)
             return
-        item['title'] = response.url.split(':')[-1]
+        item['title'] = response.url.split(':')[-1].replace('_', ' ')
         item['subcats'], item['pages'] = [], []
         sublinks = []
         for link in soup.find_all('a'):
             if link is not None and link.get('class') is not None:
                 if 'CategoryTreeLabel' in link['class']:
                     if link.string is not None:
-                        item['subcats'].append(link.string)
-                    if link.get('href') is not None:
-                        sublinks.append(link['href'])
+                        if self.check(link.string):
+                            item['subcats'].append(link.string)
+                            if link.get('href') is not None:
+                                sublinks.append(link['href'])
         for div in soup.find_all('div'):
             try:
                 flag = div.h2.contents[-1].startswith('Pages in category')
