c09f83ee339af0a6d68bbd78246ac74edc90068d
kimiyoung <kimiyoung@yeah.net>
2015-03-18 22:26:23 +0800

    bug fix

--- extract_keyterms/synom.py
+++ extract_keyterms/synom.py
@@ -6,26 +6,25 @@ def get_cursor():
     return mdb.connect('localhost', 'root', password, 'wikipedia').cursor()
 
 def desym(infile, outfile, cur):
-    appeared = set()
     fout = open(outfile, 'w')
     for line in open(infile):
+        appeared = set()
         inputs = line.strip().split('\t')
         if len(inputs) < 3: continue
         words = []
         for word in inputs[2].split('|'):
             cur.execute("select page_id, page_is_redirect from page where page_title = %s", word.capitalize())
-            flag = True
+            redirect, duplic = False, False
             for row in cur.fetchall():
                 if row[1] == 1:
+                    redirect = True
                     cur.execute("select rd_title from redirect where rd_from = %s", row[0])
-                    name = cur.fetchone()[0]
-                    if name in appeared:
-                        flag = False
-                    else:
-                        appeared.add(name)
+                    name = cur.fetchone()[0].lower()
+                    if name in appeared: duplic = True
+                    else: appeared.add(name)
                     break
-            if flag:
-                words.append(word)
+            if not duplic: words.append(word)
+            if not redirect: appeared.add(word)
         fout.write("\t".join(inputs[:2] + ["|".join(words)]) + '\n')
     fout.close()
 
