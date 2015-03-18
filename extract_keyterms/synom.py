846335d9cc614c805cd440e106383f9401a60fec
kimiyoung <kimiyoung@yeah.net>
2015-03-18 22:55:37 +0800

    test

--- extract_keyterms/synom.py
+++ extract_keyterms/synom.py
@@ -11,6 +11,7 @@ def desym(infile, outfile, cur):
         appeared = set()
         inputs = line.strip().split('\t')
         if len(inputs) < 3: continue
+        if inputs[1] != 'Jie Tang': continue ##########
         words = []
         for word in inputs[2].split('|'):
             cur.execute("select page_id, page_is_redirect from page where page_title = %s", word.capitalize())
@@ -21,15 +22,17 @@ def desym(infile, outfile, cur):
                     cur.execute("select rd_title from redirect where rd_from = %s", row[0])
                     name = cur.fetchone()[0].lower()
                     break
+            if word.startswith('Social'): print name    ##########
             if name in appeared: continue
             appeared.add(name)
             words.append(word)
-        fout.write("\t".join(inputs[:2] + ["|".join(words)]) + '\n')
+        #fout.write("\t".join(inputs[:2] + ["|".join(words)]) + '\n')   ########
     fout.close()
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     cur = get_cursor()
     for jconf in ['ASPLOS', 'SIGCOMM', 'CRYPTO', 'OSDI', 'STOC', 'SIGMOD Conference', 'KDD', 'SIGGRAPH', 'ICML', 'CHI']:
+        if jconf != 'KDD': continue ################
         logging.info('processing %s' % jconf)
         desym('res_' + jconf + '.out', 'desym_' + jconf + '.out', cur)
