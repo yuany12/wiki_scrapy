ac4759fe32cf6b3ee3b79af0791997097a9b1c0d
kimiyoung <kimiyoung@yeah.net>
2015-03-26 21:39:33 +0800

    fix arguments

--- embedding/embed.py
+++ embedding/embed.py
@@ -32,10 +32,10 @@ class author_word_embedding:
                 logging.info('building graph %d/%d' % (cnt, tot))
             cnt += 1
             keywords = ext.extract_str(row[1])
-            cur.execute("select abstract from publication_ext where id = %s", str(row[0]))
+            cur.execute("select abstract from publication_ext where id = %d" % row[0])
             for sub_row in cur.fetchone():
                 if sub_row is not None and sub_row[0] is not None and sub_row[0] != '': keywords += ext.extract_str(sub_row[0])
-            cur.execute("select aid from na_author2pub where pid = %s", str(row[0]))
+            cur.execute("select aid from na_author2pub where pid = %d" % row[0])
             for sub_row in cur.fetchall():
                 keywords.append("A_" + str(sub_row[0]))
             for i in range(len(keywords)):
