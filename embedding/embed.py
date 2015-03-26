2265799b6fafc2407b93083b0aaa9734c4d3ef1f
kimiyoung <kimiyoung@yeah.net>
2015-03-26 20:55:45 +0800

    author word embedding

---
+++ embedding/embed.py
@@ -0,0 +1,79 @@
+import MySQLdb as mdb
+import extractor
+import collections
+import gensim
+import cPickle
+import multiprocessing
+
+SIZE = 128
+WINDOW = 24
+NEGATIVE = 10
+RAN_TIMES = 100
+LENGTH = 40
+
+class author_word_embedding:
+
+    def conn_db(self, database):
+        password = open('password.txt').readline().strip()
+        db = mdb.connect('localhost', 'root', password, database)
+        db.set_character_set('utf8')
+        return db
+
+    def build_graph(self):
+        cur = self.conn_db('arnet_db').cursor()
+        cur.execute("select id, title from publication where id < 10000")
+        self.vertices = collections.defaultdict(list)
+        self.vocab = []
+        ext = extractor.extractor(conn_db('wikipedia').cursor())
+        cnt, tot = 0, cur.rowcount
+        for row in cur.fetchall():
+            if cnt % 10000 == 0:
+                logging.info('building graph %d/%d' % (cnt, tot))
+            cnt += 1
+            keywords = ext.extract_str(row[1])
+            cur.execute("select abstract from publication_ext where id = %s", row[0])
+            for sub_row in cur.fetchone():
+                if sub_row[0] is not None and sub_row[0] != '': keywords += ext.extract_str(sub_row[0])
+            cur.execute("select aid from na_author2pub where pid = %s", row[0])
+            for sub_row in cur.fetchall():
+                keywords.append("A_" + str(sub_row[0]))
+            for i in range(len(keywords)):
+                for j in range(i + 1, len(keywords)):
+                    self.vertices[keywords[i]].append(keywords[j])
+                    self.vertices[keywords[j]].append(keywords[i])
+            self.vocab.append(keywords)
+
+    def save_graph(self):
+        cPickle.dump(self.vertices, open('vertices.graph.dump', 'wb'))
+        cPickle.dump(self.vocab, open('vocab.graph.dump', 'wb'))
+
+    def load_graph(self):
+        self.vertices = cPickle.load(open('vertices.graph.dump', 'rb'))
+        self.vocab = cPickle.load(open('vocab.graph.dump', 'rb'))
+
+    def generator(self):
+        keys = self.vertices.keys()
+        random.shuffle(keys)
+        for key in keys:
+            path = [key]
+            for _ in range(LENGTH):
+                path.append(random.choice(self.vertices[path[-1]]))
+            yield path
+
+    def train_model(self):
+        model = gensim.models.Word2Vec(None, size = SIZE, window = WINDOW, min_count = 0, negative = NEGATIVE, \
+             workers = multiprocessing.cpu_count())
+        model.build_vocab(self.vocab)
+        for i in range(RAN_TIMES):
+            logging.info('training model %d/%d' % (i, RAN_TIMES))
+            model.train(self.generator())
+            model.save('author_word.model')
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    emd = author_word_embedding()
+    emd.build_graph()
+    emd.save_graph()
+    emd.train_model()
+    
---
+++ embedding/extractor.py
@@ -0,0 +1,48 @@
+
+from spacy.en import English
+import logging
+
+class extractor:
+    tagger = English()
+    forbidden = {'PRP','PRP$','VBZ', '.', ':', '``', "''", 'DT'}
+
+    def __init__(self, cur):
+        cur.execute("select page_title from page where id < 10000")
+        self.entities = []
+        cnt, tot = 0, cur.rowcount
+        for row in cur.fetchall():
+            if cnt % 10000 == 0:
+                logging.info('loading wikipedia %d/%d' % (cnt, tot))
+            cnt += 1
+            self.entities.append(row[0].lower())
+        self.entities = set(self.entities)
+
+    def pos_rule(self, tokens):
+        tags = [e.tag_ for e in tokens]
+        has_nns = False
+        for e in tags:
+            if e is None: return False
+            if e in self.forbidden: return False
+            if e.startswith('NN'): has_nns = True
+        if not has_nns: return False
+        if tags[0] == 'CD': return False
+        return True
+
+    def extract_str(self, s):
+        tokens = self.tagger(s, tag = True, parse = False)
+        i, ret = 0, []
+        while i < len(tokens) - 1:
+            if i + 2 < len(tokens) and self.pos_rule(tokens[i: i + 3]):
+                word = "_".join(e.orth_.lower() for e in tokens[i: i + 3])
+                if word in self.entities:
+                    ret.append(word)
+                    i += 3
+                    continue
+            if self.pos_rule(tokens[i: i + 2]):
+                word = "_".join(e.orth_.lower() for e in tokens[i: i + 2])
+                if word in self.entities:
+                    ret.append(word)
+                    i += 2
+                    continue
+            i += 1
+        return ret
