c3cf738a864afe37c30a1c68d991a968fa6840e9
kimiyoung <kimiyoung@yeah.net>
2015-03-29 13:13:10 +0800

    test more

--- embedding/test.py
+++ embedding/test.py
@@ -1,9 +1,18 @@
 import gensim
 
+def arnet_conn():
+    password = open('password.txt').readline().strip()
+    return mdb.connect('localhost', 'root', password, 'arnet_db')
+
 def test():
     model = gensim.models.Word2Vec.load('author_word.model')
-    for word in ['machine_learning', 'data_mining', 'deep_learning', 'social_network', 'support_vector_machine', 'neural_network']:
-        for e in model.most_similar(positive = [word], negative = [], topn = 10):
+    cur = arnet_conn().cursor()
+    for word in ['machine_learning', 'data_mining', 'deep_learning', 'social_network', 'support_vector_machine', \
+     'neural_network', 'deep_network', 'euclidean_space', 'word_embedding', 'natural_language_processing']:
+        for e in model.most_similar(positive = [word], negative = [], topn = 15):
+            if e[0].startswith('A_'):
+                cur.execute("select names from na_person where id = %s", e[0][3:])
+                e = (cur.fetchone()[0], e[1])
             print word, e
 
 if __name__ == '__main__':
