5d23a987e4292d796452479316825e420e5dae8a
kimiyoung <kimiyoung@yeah.net>
2015-04-02 19:52:10 +0800

    test author

--- embedding/test.py
+++ embedding/test.py
@@ -9,12 +9,17 @@ def test():
     model = gensim.models.Word2Vec.load('author_word.model')
     cur = arnet_conn().cursor()
     for word in ['machine_learning', 'data_mining', 'deep_learning', 'social_network', 'support_vector_machine', \
-     'neural_network', 'vector_space', 'euclidean_space', 'word_embedding', 'natural_language_processing']:
+     'neural_network', 'vector_space', 'euclidean_space', 'word_embedding', 'natural_language_processing', 'A_1458619', \
+      'A_745329', 'A_123223', 'A_386117']:
+        name = word
+        if name.startswith('A_'):
+            cur.execute("select names from na_person where id = %", int(name[2:]))
+            name = cur.fetchone()[0]
         for e in model.most_similar(positive = [word], negative = [], topn = 15):
             if e[0].startswith('A_'):
                 cur.execute("select names from na_person where id = %s", e[0][2:])
                 e = (cur.fetchone()[0], e[1])
-            print word, e
+            print name, e
 
 if __name__ == '__main__':
     test()
