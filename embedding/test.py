d8f413811014eb6fa1ba7cb32da70fe69fe22026
kimiyoung <kimiyoung@yeah.net>
2015-04-03 17:26:55 +0800

    wiki embedding

--- embedding/test.py
+++ embedding/test.py
@@ -1,26 +1,51 @@
 import gensim
 import MySQLdb as mdb
+from tsne import bh_sne
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+import string
 
 def arnet_conn():
     password = open('password.txt').readline().strip()
-    return mdb.connect('localhost', 'root', password, 'arnet_db')
+    return mdb.connect('166.111.7.105', 'root', password, 'arnet_db')
 
 def test():
     model = gensim.models.Word2Vec.load('author_word.model')
+    x, label = [], []
     cur = arnet_conn().cursor()
     for word in ['A_1458619', 'A_745329', 'A_123223', 'A_386117', 'A_1464342', 'A_826096', 'A_191749', 'A_221919', \
-     'A_161041', 'A_265966', 'A_560995', 'A_750943', 'A_14169490', 'A_1479768', 'A_340525']:
+     'A_161041', 'A_265966', 'A_560995', 'A_750943', 'A_14169490', 'A_1479768', 'A_340525', 'A_773167', 'A_173433', \
+     'A_546561', 'A_856261']:
         name = word
         if name.startswith('A_'):
             cur.execute("select names from na_person where id = %s", name[2:])
             name = cur.fetchone()[0]
+
+        x.append(model[word])
+        label.append(name)
+
         for e in model.most_similar(positive = [word], negative = [], topn = 15):
             if e[0].startswith('A_'):
+                x.append(model[e[0]])
+                label.append('')
+
                 cur.execute("select names from na_person where id = %s", e[0][2:])
                 row = cur.fetchone()
                 if row is not None and row[0] is not None:
                     e = (row[0], e[1])
             print name, e
+    x = bh_sne(np.array(x, dtype = np.float64))
+    x1, x2 = x[:, 0], x[:, 1]
+
+    fig, ax = plt.subplots()
+    ax.scatter(x1, x2)
+
+    for i in range(len(label)):
+        ax.annotate(label[i], xy = (x1[i], x2[i]))
+
+    plt.savefig('test.embedding.png', bbox_inches = 'tight')
+
 
 if __name__ == '__main__':
     test()
---
+++ embedding/wiki_embed.py
@@ -0,0 +1,66 @@
+
+import cPickle
+from collections import defaultdict as dd
+from gensim import LineSentence
+import logging
+
+SIZE = 200
+WINDOW = 10
+MIN_COUNT = 10
+SAMPLE = 1e-4
+
+TH_DB_CNT = 10
+TH_HIT_CNT = 3
+
+class wiki_embedding:
+    
+    def load_vocab(self):
+        title_keywords = cPickle.load(open('title_keywords.dump', 'rb'))
+        abs_keywords = cPickle.load(open('abs_keywords.dump', 'rb'))
+
+        self.keywords = dd(int)
+        for k, v in title_keywords.iteritems():
+            for keyword in v:
+                self.keywords[keyword] += 1
+
+    def generator(self):
+        cnt = 0
+        for line in open('../../wiki/wiki.text'):
+            if cnt % 10000 == 0:
+                logging.info('processing line %d' % cnt)
+            cnt += 1
+            inputs = line.strip().split():
+            i, hit_cnt = 0, 0
+            ret = []
+            while i + 2 <= len(inputs):
+                if i + 3 <= len(inputs):
+                    new_word = "_".join(inputs[i: i + 3])
+                    if self.keywords[new_word] > TH_DB_CNT:
+                        ret.append(new_word)
+                        i += 3
+                        hit_cnt += 1
+                        continue
+                new_word = "_".join(inputs[i: i + 2])
+                if self.keywords[new_word] > TH_DB_CNT:
+                    ret.append(new_word)
+                    i += 2
+                    hit_cnt += 1
+                    continue
+                ret.append(inputs[i])
+                i += 1
+            if hit_cnt < TH_HIT_CNT: continue
+            yield ret
+
+    def train_model(self):
+        model = gensim.models.Word2Vec(None, size = SIZE, window = WINDOW, min_count = MIN_COUNT, \
+             workers = multiprocessing.cpu_count(), sample = SAMPLE)
+        model.build_vocab(self.generator())
+        model.train(self.generator())
+        model.save('keyword.model')
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    emd = wiki_embedding()
+    emd.load_vocab()
+    emd.train_model()
+
