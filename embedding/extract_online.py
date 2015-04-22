0317b4417a31ee4f6e33d5ccaaefd774f14fcab4
kimiyoung <kimiyoung@yeah.net>
2015-04-22 20:51:40 +0800

    extract abstract p = 4

--- embedding/extract_online.py
+++ embedding/extract_online.py
@@ -2,6 +2,7 @@ import extractor
 import logging
 import cPickle
 import pymongo
+import multiprocessing
 # import MySQLdb as mdb
 
 def get_mongodb():
@@ -15,7 +16,7 @@ def get_db(database):
     password = open('password.txt').readline().strip()
     return mdb.connect('localhost', 'root', password, database).cursor()
 
-def extract_all():
+def extract_all(bulk_size = 80000000, bulk_no = 0):
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     # ext = extractor.extractor(get_db('wikipedia'))
@@ -24,20 +25,20 @@ def extract_all():
     mongodb = get_mongodb()
     pubs = mongodb.publication
     cnt, tot = 0, pubs.count()
-    for doc in pubs.find():
-        if cnt % 100 == 0:
-            logging.info("loading %d/%d" % (cnt, tot))
+    for doc in pubs.find(skip = bulk_size * bulk_no, limit = bulk_size):
+        if cnt % 100 == 0 and bulk_no == 0:
+            logging.info("loading abstract %d/%d" % (cnt, tot))
         cnt += 1
 
         id = str(doc['_id'])
-        title = doc['title'] if 'title' in doc else ''
-        # abs = doc['abstract'] if 'abstract' in doc else ''
+        # title = doc['title'] if 'title' in doc else ''
+        abs = doc['abstract'] if 'abstract' in doc else ''
 
-        title_keywords = ext.extract_str(title)
-        # abstract_keywords = ext.extract_str(abs)
+        # title_keywords = ext.extract_str(title)
+        abstract_keywords = ext.extract_str(abs)
 
-        pubs.update_one({'_id': doc['_id']}, {'$set': {'title_keywords': title_keywords}})#,\
-         # 'abstract_keywords': abstract_keywords}})
+        # pubs.update_one({'_id': doc['_id']}, {'$set': {'title_keywords': title_keywords}})
+        pubs.update_one({'_id': doc['_id']}, {'$set': {'abstract_keywords': abstract_keywords}})
 
     # logging.info('dumping title_keywords')
     # cPickle.dump(title_keywords, open('title_keywords.dump', 'wb'), protocol = 2)
@@ -46,5 +47,6 @@ def extract_all():
     # cPickle.dump(abs_keywords, open('abs_keywords.dump', 'wb'), protocol = 2)
 
 if __name__ == '__main__':
-    extract_all()
+    pool = multiprocessing.Pool(processes = 4)
+    pool.map(extract_all, [(18500000, i) for i in range(4)])
 
