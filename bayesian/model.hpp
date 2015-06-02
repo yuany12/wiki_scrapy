d6a7965e17c03dee887f679ec8f4cbf175ccf588
kimiyoung <kimiyoung@yeah.net>
2015-06-02 22:57:12 +0800

    fix float conversion bug

--- bayesian/model.hpp
+++ bayesian/model.hpp
@@ -373,11 +373,8 @@ public:
         float ret = 0.0;
         int n = n_r_t[t];
         ret += log_gamma_ratio(n + dn, n);
-        if (t == 0 && dn == 1) cout << "log_gamma_ratio = " << log_gamma_ratio(n + dn, n) << endl;
-        ret += 0.5 * log2(n / (n + dn));
-        if (t == 0 && dn == 1) cout << "log2 = " << 0.5 * (log2(n) - log(n + dn)) << endl;
+        ret += 0.5 * (log2(n) - log2(n + dn));
         ret += 0.5 * dn * LOG_2_PI;
-        if (t == 0 && dn == 1) cout << "log 2 pi = " << 0.5 * dn * LOG_2_PI << endl;
         return ret;
     }
 
