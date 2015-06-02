0d96d0d3962f7d630176da788d4a6d41d7f08004
kimiyoung <kimiyoung@yeah.net>
2015-06-02 12:43:43 +0800

    imp

--- bayesian/logging.hpp
+++ bayesian/logging.hpp
@@ -9,5 +9,7 @@ using namespace std;
 void logging(const char* s) {
     auto t = time(NULL);
     auto tm = *localtime(&t);
-    cout << asctime(&tm) << s << endl;
+    char * ctime = asctime(&tm);
+    ctime[strlen(ctime) - 1] = ' ';
+    cout << ctime << s << endl;
 }
