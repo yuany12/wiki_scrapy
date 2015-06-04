25f394e32edf1b632538232a3095e3ee25bcea4c
kimiyoung <kimiyoung@yeah.net>
2015-06-04 13:56:49 +0800

    ban r

--- bayesian/model.hpp
+++ bayesian/model.hpp
@@ -107,7 +107,7 @@ public:
     int * n_k_t, * n_r_t;
     float ** sqr_k, ** sum_k, ** sqr_r, ** sum_r;
 
-    float lr_r = 1e-3, lr_k = 1e-2; // learning rate for embedding update
+    float lr_r = 0, lr_k = 1e-2; // learning rate for embedding update
     const int emb_max_iter = 5;
 
     const int learning_max_iter = 10;
