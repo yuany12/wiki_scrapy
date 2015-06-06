46e49e58a6b0ced30e86df38f80df73e4f1afc32
kimiyoung <kimiyoung@yeah.net>
2015-06-06 11:55:43 +0800

    sample topics only

--- bayesian/main.cpp
+++ bayesian/main.cpp
@@ -38,10 +38,6 @@ int main() {
     fclose(fin);
 
     m.sample_topics();
-    m.embedding_update();
-    m.sample_topics();
-    m.embedding_update();
-    m.sample_topics();
 
     char buffer[200];
     FILE * fout;
