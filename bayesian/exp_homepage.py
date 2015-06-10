4fdb87fcde7acd9c440c3d01bd37d9fcda0f1c5d
kimiyoung <kimiyoung@yeah.net>
2015-06-10 23:05:57 +0800

    test

--- bayesian/exp_homepage.py
+++ bayesian/exp_homepage.py
@@ -112,33 +112,24 @@ def test_old():
 
     people = get_mongodb().people
 
-    target_authors = []
     rt, rt_cnt = 0.0, 0
-    for filename in os.listdir('../homepage'):
-        author = filename.split('.')[0]
-        if author not in author2words: continue
+    for line in open('homepage_test.txt'):
+        inputs = line.strip().split(',')
+        author = inputs[0]
 
-        doc = open('../homepage/' + filename).read()
-        soup = BeautifulSoup(doc)
-        doc = soup.get_text()
-
-        people_doc = people.find_one({'_id': ObjectId(author)})
+        doc = people.find_one({'_id': ObjectId(author)})
 
         pos_cnt, neg_cnt = 0, 0
 
-        for keyword in people_doc['tags'][: 5]:
+        for keyword in doc['tags'][: 5]:
             keyword = keyword['t']
-            if keyword.replace('_', ' ') in doc: pos_cnt += 1
+            if keyword in keywords: pos_cnt += 1
             else: neg_cnt += 1
-
-        rt += 1.0 * pos_cnt / (pos_cnt + neg_cnt) if pos_cnt + neg_cnt > 0 else 0.0
+        rt += 1.0 * pos_cnt / (pos_cnt + neg_cnt)
         rt_cnt += 1
-
         print rt / rt_cnt
 
-    print rt / rt_cnt
-
 if __name__ == '__main__':
-    test_bayesian()
-    # test_old()
+    # test_bayesian()
+    test_old()
     # gen_()
